363eb22d09ed2b18af54f2caa6bf63df62c8988e
answaj97 <answaj97@gmail.com>
2022-02-03 18:53:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var a = function(){
-    
-  var b = function(){
-    console.log("b");  
-  }
+var a = function () {
+  var b = function () {
+    console.log('b');
+  };
   b();
-  var c= function(){
-      console.log("c");
-  }
+  var c = function () {
+    console.log('c');
+  };
   c();
-}
+};
 a();
